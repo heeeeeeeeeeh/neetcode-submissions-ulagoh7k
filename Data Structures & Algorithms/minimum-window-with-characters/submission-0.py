f2abe693bf40387a579f9c0f127a771f3d112930
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-05-09 17:35:11 -0500

    Update: minimum-window-with-characters - submission-0

--- Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -1,27 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
+        if len(s) < len(t):
             return ""
 
-        countT = defaultdict(int)
-        for i in range(len(t)):
-            countT[t[i]] += 1
+        tCount = defaultdict(int)
+        for c in t:
+            tCount[c] += 1
         
+        sCount = defaultdict(int)
         res, resLen = [-1,-1], float("inf")
-        for i in range(len(s)):
-            countS = defaultdict(int)
-            for j in range(i,len(s)):
-                countS[s[j]] += 1
+        have, need = 0, len(tCount)
+        l = 0
+        for r in range(len(s)):
+            sCount[s[r]] +=1
 
-                flag = True
-                for c in countT:
-                    if countT[c] > countS[c]:
-                        flag = False
-                        break
-                
-                if flag and (j-i+1) < resLen:
-                    resLen = (j-i+1)
-                    res = [i, j]
-        l, r = res
-        return s[l:r + 1] if resLen != float("inf") else ""
+            if s[r] in tCount and sCount[s[r]] == tCount[s[r]]:
+                have += 1
             
+            while need == have:
+                if r-l+1 < resLen:
+                    resLen = r-l+1
+                    res = [l, r]
+                sCount[s[l]] -= 1
+                if s[l] in tCount and sCount[s[l]] < tCount[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l:r+1] if resLen != float("inf") else ""
