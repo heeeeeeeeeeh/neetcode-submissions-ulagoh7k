b52508cd965a300a01eb43576efc4538f1eb5688
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-05-16 13:15:18 -0500

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closing = {
+            ')': "(",
+            '}':"{",
+            ']': "[",
+        }
+        stack = []
+
+        for c in s:
+            if c in closing:
+                if not stack or stack[-1] != closing[c]:
+                    return False
+                stack.pop()
+            elif c in closing.values():
+                stack.append(c)
+        return len(stack) == 0
