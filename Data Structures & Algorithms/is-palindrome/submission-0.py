5f8a5db0d682caef1f5f347be55aa55204bba994
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-05-16 11:26:49 -0500

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.py
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s)-1
 
-        while l <= r:
+        while l<=r:
             if not s[l].isalnum():
                 l += 1
                 continue
@@ -10,7 +10,7 @@ class Solution:
                 r -= 1
                 continue
             
-            if not s[l].lower() == s[r].lower():
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
