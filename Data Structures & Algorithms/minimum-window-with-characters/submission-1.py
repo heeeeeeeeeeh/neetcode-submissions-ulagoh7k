393f07c631a2d4a13f86f310cd2f828961128248
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-05-16 12:53:00 -0500

    Update: minimum-window-with-characters - submission-1

--- Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -1,25 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
-            return "";
+        if len(s) < len(t):
+            return ""
+        
+        res, resLen = [-1,-1], float("inf")
 
         tCount = defaultdict(int)
+        sCount = defaultdict(int)
         for c in t:
             tCount[c] += 1
         
-        res, resLen = [-1,-1], float("inf")
-        for i in range(len(s)):
-            sCount = defaultdict(int)
-            for j in range(i,len(s)):
-                sCount[s[j]] += 1
+        have, need = 0, len(tCount)
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            sCount[c] += 1
 
-                flag = True
-                for c in tCount:
-                    if sCount[c] < tCount[c]:
-                        flag = False
-                        break
-                if flag and (j-i+1) < resLen:
-                    resLen = j-i+1
-                    res = [i, j]
+            if c in tCount and sCount[c] == tCount[c]:
+                have += 1
+            
+            while have == need:
+                if (r-l+1) < resLen:
+                    res = [l, r]
+                    resLen = r-l+1
+
+                sCount[s[l]] -= 1
+                if s[l] in tCount and sCount[s[l]] < tCount[s[l]]:
+                    have -= 1
+                l += 1
         l, r = res
-        return s[l:r + 1] if resLen != float("inf") else ""
+        return s[l:r+1] if resLen != float("inf") else ""
