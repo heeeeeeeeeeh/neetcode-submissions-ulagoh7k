d8bf5b91c982c0ded1d82be65a6f3ea5f6f7f82d
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-07 17:43:22 -0500

    Update: is-anagram - submission-1

--- Data Structures & Algorithms/is-anagram/submission-1.py
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -4,8 +4,8 @@ class Solution:
             return False
         sCount = [0]*26
         tCount = [0]*26
-
         for i in range(len(s)):
             sCount[ord(s[i]) - ord('a')] += 1
             tCount[ord(t[i]) - ord('a')] += 1
-        return tCount == sCount
+        
+        return sCount == tCount
