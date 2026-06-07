bc3f1ef28d1ff4b4e370070471709ab4729228af
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-07 18:40:27 -0500

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.py
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -6,10 +6,9 @@ class Solution:
             if not s[l].isalnum():
                 l += 1
                 continue
-            if not s[r].isalnum():
+            elif not s[r].isalnum():
                 r -= 1
                 continue
-            
             if s[l].lower() != s[r].lower():
                 return False
             l += 1
