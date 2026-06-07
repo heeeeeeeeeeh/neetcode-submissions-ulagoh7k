6fcff0a47394e986a41afb1541ccf5267899f500
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-07 18:35:32 -0500

    Update: valid-sudoku - submission-0

--- Data Structures & Algorithms/valid-sudoku/submission-0.py
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -9,12 +9,11 @@ class Solution:
                 num = board[r][c]
                 if num == ".":
                     continue
-                
                 if (num in rows[r] or
                     num in cols[c] or
-                    num in boxes[(r//3, c//3)]):
+                    num in boxes[(r // 3, c // 3)]):
                     return False
                 rows[r].add(num)
                 cols[c].add(num)
-                boxes[(r//3,c//3)].add(num)
-        return True 
+                boxes[(r//3, c//3)].add(num)
+        return True
