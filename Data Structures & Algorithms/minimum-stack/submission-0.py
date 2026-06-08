f6cd6c58f0fb97593f37d658e2e9768033e6c588
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-08 10:29:05 -0500

    Update: minimum-stack - submission-0

--- Data Structures & Algorithms/minimum-stack/submission-0.py
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -6,16 +6,14 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if self.min:
-            self.min.append(min(val,self.min[-1]))
-        else:
+        if not self.min:
             self.min.append(val)
-        
+        else:
+            self.min.append(min(val, self.min[-1]))
 
     def pop(self) -> None:
         self.stack.pop()
         self.min.pop()
-        
 
     def top(self) -> int:
         return self.stack[-1]
@@ -23,3 +21,4 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min[-1]
+        
