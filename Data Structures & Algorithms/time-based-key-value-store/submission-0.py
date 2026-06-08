6fc62d090924f75b1a9e725cb6f9a21c97367823
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-08 13:19:38 -0500

    Update: time-based-key-value-store - submission-0

--- Data Structures & Algorithms/time-based-key-value-store/submission-0.py
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -5,7 +5,8 @@ class TimeMap:
         
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.map[key].append((timestamp, value))
+        self.map[key].append((value, timestamp))
+        
 
     def get(self, key: str, timestamp: int) -> str:
         res = ""
@@ -13,13 +14,11 @@ class TimeMap:
             l, r = 0, len(self.map[key])-1
             while l <= r:
                 mid = l + (r-l)//2
-
-                if self.map[key][mid][0] == timestamp:
-                    return self.map[key][mid][1]
-                elif self.map[key][mid][0] > timestamp:
+                if self.map[key][mid][1] == timestamp:
+                    return self.map[key][mid][0]
+                elif self.map[key][mid][1] > timestamp:
                     r = mid - 1
                 else:
+                    res = self.map[key][mid][0]
                     l = mid + 1
-                    res = self.map[key][mid][1]
         return res
-        
