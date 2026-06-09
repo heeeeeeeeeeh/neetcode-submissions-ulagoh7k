d3340068bea7cb1cd86091ac4758460c55c69121
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-09 12:06:33 -0500

    Update: subtree-of-a-binary-tree - submission-0

--- Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -1,18 +1,26 @@
-class Solution:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
+class Solution:   
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not subRoot:
             return True
         if not root:
             return False
-        
-        if self.sameTree(root, subRoot):
+
+        if self.isSameTree(root, subRoot):
             return True
-        
-        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-    def sameTree(self, root, subRoot):
-        if not subRoot and not root:
+        else:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
             return True
-        if subRoot and root and subRoot.val == root.val:
-            return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
-        return False
+        elif p and q and p.val == q.val:
+            return (self.isSameTree(p.left, q.left) and
+                    self.isSameTree(p.right, q.right))
+        else:
+            return False
