feabc379aa8831541cf486fdf6037bdb36e21227
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-14 13:27:06 -0500

    Update: trapping-rain-water - submission-0

--- Data Structures & Algorithms/trapping-rain-water/submission-0.py
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -7,9 +7,9 @@ class Solution:
             if maxL < maxR:
                 l += 1
                 maxL = max(maxL, height[l])
-                res += maxL-height[l]
+                res += maxL - height[l]
             else:
                 r -= 1
                 maxR = max(maxR, height[r])
-                res += maxR-height[r]
+                res += maxR - height[r]
         return res
