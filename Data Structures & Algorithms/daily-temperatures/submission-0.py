e0f95ccab74a3d4fe8ea8525ac5e9250216f54f0
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-14 14:20:17 -0500

    Update: daily-temperatures - submission-0

--- Data Structures & Algorithms/daily-temperatures/submission-0.py
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -5,6 +5,7 @@ class Solution:
         for i, t in enumerate(temperatures):
             while stack and stack[-1][1] < t:
                 stackI, stackT = stack.pop()
-                res[stackI] = i-stackI
-            stack.append((i, t))
+                res[stackI] = i - stackI
+            stack.append((i,t))
         return res
+            
