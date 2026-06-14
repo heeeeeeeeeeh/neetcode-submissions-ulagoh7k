e1388741718b5f89082f489631a518e9bb558125
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-14 13:14:32 -0500

    Update: two-integer-sum-ii - submission-0

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -5,7 +5,8 @@ class Solution:
             s = numbers[l] + numbers[r]
             if s == target:
                 return [l + 1, r + 1]
-            elif s > target:
-                r -= 1
-            else:
+            elif s < target:
                 l += 1
+            else:
+                r -= 1
+        
