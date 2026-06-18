d37965003ee29fa9acc784496da8b6caad86edea
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-18 14:33:24 -0500

    Update: linked-list-cycle-detection - submission-1

--- Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
@@ -6,11 +6,10 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        fast, slow = head, head
-
+        slow = fast = head
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-            if fast == slow:
+            if slow == fast:
                 return True
         return False
