5129384cad3c1e5fd491c57eb2364267fea6a695
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-20 16:55:23 -0500

    Update: kth-smallest-integer-in-bst - submission-0

--- Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.py
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.py
@@ -8,7 +8,6 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         cur = root
-
         while cur:
             if not cur.left:
                 k -= 1
@@ -19,11 +18,11 @@ class Solution:
                 prev = cur.left
                 while prev.right and prev.right != cur:
                     prev = prev.right
+                
                 if not prev.right:
                     prev.right = cur
                     cur = cur.left
                 else:
-                    prev.right = None
                     k -= 1
                     if k == 0:
                         return cur.val
