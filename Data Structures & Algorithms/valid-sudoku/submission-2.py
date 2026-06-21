e2e72d8cb8e5dc1c5a684e30520ed5cf2636a201
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-21 13:14:43 -0500

    Update: valid-sudoku - submission-2

--- Data Structures & Algorithms/valid-sudoku/submission-2.py
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -1,58 +1,19 @@
 class Solution:
-    #Understand: check if sudoku board is vaild
-    #Plan:
-    #row validation
-        #loop though row in board
-            # init a hash for prev numbers in row
-            #loop though each col in board
-                # if current number is in prev number hash and not . return false
-                # else put cur num in hashmap
-    # col validation
-        #loop though row in board
-            #loop though col in board
-                # if current number is not . and is in prev number
-                    # return false
-                # else 
-                    # put cur num in hashmap
-    # loop though each row of SubBoards 
-        # loop though col of subboards
-            # loop though subboard row
-                # loop though each subboard col
-                    # init hash for prev numbers in subboard
-                    # loop though each col
-                        # if current number is in prev num
-                            # else put cur num in hashmap
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in range(len(board)):
-            preNum = {}
-            for col in range(len(board[row])):
-                curNum = board[row][col]
-                if curNum != "." and curNum in preNum:
-                    return False
-                else:
-                    preNum[curNum] = True
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = defaultdict(set)
 
-        for col in range(len(board)):
-            preNum = {}
-            for row in range(len(board)):
-                curNum = board[row][col]
-                if curNum != "." and curNum in preNum:
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                if num == ".":
+                    continue
+                if (num in rows[r] or
+                    num in cols[c] or
+                    num in boxes[(r//3, c//3)]):
                     return False
-                else:
-                    preNum[curNum] = True
-        
-        for subboardRow in range(0,9,3):
-            print(f"Subboard row: {subboardRow}")
-            for subboardCol in range(0,9,3):
-                print(f"\tSubboard col: {subboardCol}")
-                preNum = {}
-                for row in range(3):
-                    print(f"\t\tBoard row: {subboardRow+row}")
-                    for col in range(3):
-                        print(f"\t\t\tBoard col: {subboardCol+col}")
-                        curNum = board[subboardRow+row][subboardCol+col]
-                        if curNum != "." and curNum in preNum:
-                            return False
-                        else:
-                            preNum[curNum] = True
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[(r//3, c//3)].add(num)
         return True
