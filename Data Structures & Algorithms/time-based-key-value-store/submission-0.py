80b48b96c5fa52ae5f462a09f0b8785df0e0d447
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-21 14:55:07 -0500

    Update: time-based-key-value-store - submission-0

--- Data Structures & Algorithms/time-based-key-value-store/submission-0.py
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -16,9 +16,9 @@ class TimeMap:
                 mid = l + (r-l)//2
                 if arr[mid][1] == timestamp:
                     return arr[mid][0]
-                if arr[mid][1] > timestamp:
-                    r = mid - 1
-                else:
+                elif arr[mid][1] < timestamp:
                     res = arr[mid][0]
                     l = mid + 1
+                else:
+                    r = mid - 1
         return res
