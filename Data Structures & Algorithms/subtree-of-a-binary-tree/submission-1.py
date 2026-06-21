4b3b7900285f7231f609deb759d1811552543dc0
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-21 16:33:12 -0500

    Update: subtree-of-a-binary-tree - submission-1

--- Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.py
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.py
@@ -1,18 +1,35 @@
-class Solution:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:
-            return True
+class Solution:
+    def serialize(self, root):
         if not root:
-            return False
-        
-        if self.sameTree(root, subRoot):
-            return True
-        
-        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-    def sameTree(self, root, subRoot):
-        if not subRoot and not root:
-            return True
-        if subRoot and root and subRoot.val == root.val:
-            return self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right)
+            return "$#"
+        return ("$" + str(root.val)
+                + self.serialize(root.left)
+                + self.serialize(root.right))
+    def z_function(self, s):
+        z = [0]*len(s)
+        l = r = 0
+        for i in range(1, len(s)):
+            if i <= r:
+                z[i] = min(z[l-i], r-i+1)
+            while i + z[i] < len(s) and s[z[i]] == s[i + z[i]]:
+                z[i] += 1
+            if i + z[i] - 1 > r:
+                l, r = i, i + z[i] - 1
+        return z
+    
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        subS, rootS = self.serialize(subRoot), self.serialize(root)
+        combined = subS + "|" +  rootS
+        z = self.z_function(combined)
+        for i in range(len(subS) + 1, len(combined)):
+            if z[i] == len(subS):
+                return True
         return False
+        
