fe94460858b2d3a2972c81bc864aa57eb71afde7
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-21 15:50:17 -0500

    Update: merge-k-sorted-linked-lists - submission-0

--- Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.py
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.py
@@ -9,14 +9,13 @@ class Solution:
         if not lists or len(lists) == 0:
             return None
         while len(lists) > 1:
-            mergedList = []
-            for i in range(0,len(lists), 2):
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
-                mergedList.append(self.mergeLists(lists[i], l2))
-            lists = mergedList
+            mergedLists = []
+            for i in range(0, len(lists),2):
+                l2  = lists[i + 1] if i + 1 < len(lists) else None
+                mergedLists.append(self.mergeList(lists[i], l2))
+            lists = mergedLists
         return lists[0]
-
-    def mergeLists(self, l1, l2):
+    def mergeList(self, l1, l2):
         dummy = cur = ListNode()
         while l1 and l2:
             if l1.val < l2.val:
@@ -27,4 +26,4 @@ class Solution:
                 l2 = l2.next
             cur = cur.next
         cur.next = l1 or l2
-        return dummy.next
+        return dummy.next 
