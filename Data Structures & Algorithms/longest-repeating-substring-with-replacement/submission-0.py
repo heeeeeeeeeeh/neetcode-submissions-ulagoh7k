db59e7e931c4401251dcaaf38fd5d2638e5ea3f5
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-21 13:41:32 -0500

    Update: longest-repeating-substring-with-replacement - submission-0

--- Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -7,8 +7,9 @@ class Solution:
         for r in range(len(s)):
             count[s[r]] += 1
             maxf = max(maxf, count[s[r]])
-            while (r-l+1)-maxf > k:
+
+            while r-l+1-maxf > k:
                 count[s[l]] -= 1
                 l += 1
-            maxL = max(maxL, (r-l) + 1)
+            maxL = max(maxL, r-l+1)
         return maxL
