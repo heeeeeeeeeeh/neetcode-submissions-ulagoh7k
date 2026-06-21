bd5d615beb00396e6d1ef5dfb29abe058f6e5c8d
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-21 14:03:51 -0500

    Update: minimum-window-with-characters - submission-0

--- Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -2,14 +2,11 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
-        
+
         res, resLen = [-1,-1], float("inf")
-        countS, countT = defaultdict(int), defaultdict(int)
-
-        for i in range(len(t)):
-            countT[t[i]] += 1
+        
+        countT, countS = Counter(t), defaultdict(int)
         have, need = 0, len(countT)
-
         l = 0
         for r in range(len(s)):
             c = s[r]
@@ -18,13 +15,14 @@ class Solution:
                 have += 1
             
             while have == need:
-                if (r-l+1) < resLen:
+                if r-l+1 < resLen:
                     resLen = r-l+1
                     res = [l, r]
                 c = s[l]
                 countS[c] -= 1
-                l += 1
-                if c in countT and countS[c] == countT[c] - 1:
+                if c in countT and countS[c] + 1 == countT[c]:
                     have -= 1
+                l += 1
         l, r = res
-        return s[l:r + 1] if resLen != float("inf") else ""
+        return s[l:r + 1] if resLen != float("inf") else "" 
+            
