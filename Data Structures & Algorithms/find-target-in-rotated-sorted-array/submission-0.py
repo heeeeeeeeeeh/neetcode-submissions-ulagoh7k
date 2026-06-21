f5e8c454dbb815780d801dae347c5764eed09619
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-21 14:51:05 -0500

    Update: find-target-in-rotated-sorted-array - submission-0

--- Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.py
@@ -6,14 +6,14 @@ class Solution:
             mid = l + (r-l)//2
             if nums[mid] == target:
                 return mid
-            if nums[mid] >= nums[r]:
+            elif nums[mid] >= nums[l]:
                 if target > nums[mid] or target < nums[l]:
                     l = mid + 1
                 else:
                     r = mid -1
             else:
-                if target < nums[mid] or target > nums[r]:
+                if target > nums[r] or target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
-        return -1 
+        return -1
