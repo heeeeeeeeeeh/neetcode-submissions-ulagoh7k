ad08307eda02c7725de2907c3dcdf1f84e5d1c0b
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-25 15:45:30 -0500

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.py
@@ -0,0 +1,55 @@
+class Heap:
+    def __init__(self, nums):
+        self.buildHeap(nums)
+    
+    def buildHeap(self, nums):
+        self.nums = nums
+        for i in range(len(nums)-1, -1,-1):
+            self.heapifyDown(i)
+    
+    def heapifyDown(self, i):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        m = i
+        
+        if left < len(self.nums) and self.nums[left] < self.nums[m]:
+            m = left
+        if right < len(self.nums) and self.nums[right] < self.nums[m]:
+            m = right
+        
+        if i != m:
+            self.nums[i], self.nums[m] = self.nums[m], self.nums[i]
+            self.heapifyDown(m)
+    
+    def heapifyUp(self, i):
+        parent = (i-1)//2
+        if i > 0 and self.nums[i] < self.nums[parent]:
+            self.nums[i], self.nums[parent] = self.nums[parent], self.nums[i]
+            self.heapifyUp(parent)
+    
+    def insert(self, val):
+        self.nums.append(val)
+        self.heapifyUp(len(self.nums)-1)
+    
+    def pop(self):
+        if len(self.nums) == 1:
+            return self.nums.pop()
+        val, self.nums[0] = self.nums[0], self.nums.pop()
+        self.heapifyDown(0)
+        return val
+    
+    def getMin(self):
+        return self.nums[0]
+    
+    def __len__(self):
+        return len(self.nums)
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-s for s in stones]
+        heap = Heap(stones)
+        while len(heap) > 1:
+            first, second = heap.pop(), heap.pop()
+            if second > first:
+                heap.insert(first-second)
+        return -stones[0] if len(heap) > 0 else 0
