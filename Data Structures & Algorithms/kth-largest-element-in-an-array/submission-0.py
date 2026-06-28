3a1b3980c14b989fbaddd9c5c19c086f9638ca57
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-28 13:49:27 -0500

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.py
@@ -0,0 +1,50 @@
+class Heap:
+    def __init__(self, nums):
+        self.buildHeap(nums)
+    
+    def buildHeap(self, nums):
+        self.nums = nums
+        for i in range(len(nums)-1, -1,-1):
+            self.heapifyDown(i)
+    
+    def heapifyDown(self, i):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        m = i
+        
+        if left < len(self.nums) and self.nums[left] < self.nums[m]:
+            m = left
+        if right < len(self.nums) and self.nums[right] < self.nums[m]:
+            m = right
+        
+        if i != m:
+            self.nums[i], self.nums[m] = self.nums[m], self.nums[i]
+            self.heapifyDown(m)
+    
+    def heapifyUp(self, i):
+        parent = (i-1)//2
+        if i > 0 and self.nums[i] < self.nums[parent]:
+            self.nums[i], self.nums[parent] = self.nums[parent], self.nums[i]
+            self.heapifyUp(parent)
+    
+    def insert(self, val):
+        self.nums.append(val)
+        self.heapifyUp(len(self.nums)-1)
+    
+    def pop(self):
+        self.nums[0] = self.nums.pop()
+        self.heapifyDown(0)
+    
+    def getMin(self):
+        return self.nums[0]
+    
+    def __len__(self):
+        return len(self.nums)
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = Heap(nums)
+        while len(heap) > k:
+            heap.pop()
+        return nums[0]
+        
