1ec88b09f1eaf134fae19d271f363789baf052ab
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-01 17:56:35 -0500

    Update: eating-bananas - submission-0

--- Data Structures & Algorithms/eating-bananas/submission-0.py
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -1,7 +1,7 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l, r = 1, max(piles)
-        minS = 0
+        minS = r
         
         while l <= r:
             k = l + (r-l)//2
