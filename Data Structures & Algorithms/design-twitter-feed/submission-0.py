5c8a53539849bd455d428143e83e5027fb1e0e07
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-06-30 19:07:03 -0500

    Update: design-twitter-feed - submission-0

--- Data Structures & Algorithms/design-twitter-feed/submission-0.py
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.py
@@ -1,49 +1,26 @@
 class Twitter:
 
     def __init__(self):
-        self.count = 0
-        self.tweetMap = defaultdict(list)
+        self.count = 1
+        self.tweetMap = defaultdict(lambda : deque(maxlen=10))
         self.followMap = defaultdict(set)
         
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweetMap[userId].append((self.count, tweetId))
-        self.count -= 1
+        self.tweetMap[userId].appendleft((self.count, tweetId))
+        self.count += 1
+        
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        minHeap = []
-        res = []
-        self.followMap[userId].add(userId)
+        posts = (self.tweetMap[followeeId] for followeeId in ({userId} | self.followMap[userId]))
+        heap = heapq.merge(*posts, reverse=True)
+        return [tweetId for _, tweetId in islice(heap, 10)]
         
-        if len(self.followMap) >= 10:
-            maxHeap = []
-            for followeeId in self.followMap[userId]:
-                if followeeId in self.tweetMap:
-                    count, tweetId = self.tweetMap[followeeId][-1]
-                    index = len(self.tweetMap[followeeId]) - 1
-                    heapq.heapush(maxHeap, [-count, tweetId, followeeId, index -1])
-                    if len(maxHeap) > 10:
-                        heapq.heappop(maxHeap)
-            while maxHeap:
-                count, tweetId, followeeId, index = heapq.heappop(maxHeap)
-                heapq.heappush(minHeap, [-count, tweetId, followeeId, index])
-        else:
-            for followeeId in self.followMap[userId]:
-                if followeeId in self.tweetMap:
-                    count, tweetId = self.tweetMap[followeeId][-1]
-                    index = len(self.tweetMap[followeeId]) - 1
-                    heapq.heappush(minHeap, [count, tweetId, followeeId, index -1])
-        while len(res) < 10 and minHeap:
-            count, tweetId, followeeId, index = heapq.heappop(minHeap)
-            res.append(tweetId)
-            if index >= 0:
-                count, tweetId = self.tweetMap[followeeId][index]
-                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
-        return res     
 
     def follow(self, followerId: int, followeeId: int) -> None:
         if followerId != followeeId:
             self.followMap[followerId].add(followeeId)
+        
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         if followeeId in self.followMap[followerId]:
