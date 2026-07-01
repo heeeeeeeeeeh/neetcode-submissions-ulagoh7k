adeb87b8f5bf2e9b48b54fd273e55edb3c8cd2af
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-01 16:53:25 -0500

    Update: three-integer-sum - submission-0

--- Data Structures & Algorithms/three-integer-sum/submission-0.py
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -3,21 +3,21 @@ class Solution:
         res = []
         nums.sort()
         for i in range(len(nums)):
-            if i >= 0 and nums[i] > 0:
+            if nums[i] > 0:
                 break
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
             j, k = i + 1, len(nums)-1
             while j < k:
-                s = nums[i] + nums[j] + nums[k]
+                s = nums[i] + nums[j]+nums[k]
                 if s == 0:
-                    res.append([nums[i], nums[j], nums[k]])
+                    res.append([nums[i], nums[j],  nums[k]])
                     j += 1
                     k -= 1
-                    while j < k and nums[j] == nums[j-1]:
+                    while j < k and nums[j-1] == nums[j]:
                         j += 1
-                elif s > 0:
-                    k -= 1
-                else:
+                elif s < 0:
                     j += 1
+                else:
+                    k -= 1
         return res
