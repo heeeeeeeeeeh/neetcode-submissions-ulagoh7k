625d80d568df30c0538a15f152faaea4dded5e8d
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-01 17:19:16 -0500

    Update: permutation-string - submission-1

--- Data Structures & Algorithms/permutation-string/submission-1.py
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -2,13 +2,13 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        s1Count, s2Count = [0] * 26, [0] * 26
-
-
-        matches = 0
+        
+        s1Count, s2Count = [0]*26, [0]*26
         for i in range(len(s1)):
             s1Count[ord(s1[i]) - ord('a')] += 1
             s2Count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
         for i in range(26):
             matches += 1 if s1Count[i] == s2Count[i] else 0
         
@@ -18,16 +18,16 @@ class Solution:
                 break
             index = ord(s2[r]) - ord('a')
             s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
+            if s2Count[index] == s1Count[index]:
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+            elif s2Count[index] == s1Count[index] + 1:
                 matches -= 1
             
             index = ord(s2[l]) - ord('a')
             s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] -1 == s2Count[index]:
-                matches -= 1
+            if s2Count[index] == s1Count[index]:
+                matches +=1
+            elif s2Count[index] == s1Count[index]-1:
+                matches -=1
             l += 1
         return matches == 26
