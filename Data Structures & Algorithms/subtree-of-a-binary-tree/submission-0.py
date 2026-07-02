028d910df2a5c63ee01ee2fce4ea7e51dc128cc8
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-02 12:39:28 -0500

    Update: subtree-of-a-binary-tree - submission-0

--- Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -5,32 +5,32 @@
 #         self.left = left
 #         self.right = right
 
-class Solution:
-    def serialize(self, root):
-        if not root:
-            return "$#"
-        return ("$" + str(root.val)
-                + self.serialize(root.left)
-                + self.serialize(root.right))
-                
-    def z_function(self, s):
-        z = [0]*len(s)
-        l = r = 0
-        for i in range(1, len(s)):
-            if i <= r:
-                z[i] = min(z[l-i], r-i+1)
-            while i + z[i] < len(s) and s[z[i]] == s[i + z[i]]:
-                z[i] += 1
-            if i + z[i] - 1 > r:
-                l, r = i, i + z[i] - 1
-        return z
-    
+class Solution:   
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        subS, rootS = self.serialize(subRoot), self.serialize(root)
-        combined = subS + "|" +  rootS
+        rootS = self.serialize(root)
+        subS = self.serialize(subRoot)
+        combined = subS + "|" + rootS
         z = self.z_function(combined)
-        for i in range(len(subS) + 1, len(combined)):
+        for i in range(len(subS)+ 1, len(combined)):
             if z[i] == len(subS):
                 return True
         return False
-        
+    
+    def serialize(self, root):
+        if not root:
+            return "#"
+        return (str(root.val) + 
+                self.serialize(root.left) +
+                self.serialize(root.right))
+    
+    def z_function(self, s):
+        l = r = 0
+        z = [0]*len(s)
+        for i in range(1, len(s)):
+            if i <= r:
+                z[i] = min(z[i-l], r-i+1)
+            while i + z[i] < len(s) and s[z[i]] == s[i + z[i]]:
+                z[i] += 1
+            if i + z[i] - 1 > r:
+                l, r = i, i + z[i]-1
+        return z
