d4afd0eb2dcea804a216ce649b3bc0b6bd45f893
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-02 11:47:17 -0500

    Update: lru-cache - submission-1

--- Data Structures & Algorithms/lru-cache/submission-1.py
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -1,17 +1,27 @@
 class Node:
-    def __init__(self, key=0, value=0, nxt=None, prev=None):
-        self.key = key
-        self.val = value
-        self.next = nxt
-        self.prev = prev
+    def __init__(self, key=0, val=0, nxt=None, prev=None):
+        self.key, self.val = key, val
+        self.next, self.prev = nxt, prev
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
         self.cache = {}
+        self.cap = capacity
+        
         self.left, self.right = Node(), Node()
         self.left.next, self.right.prev = self.right, self.left
+    
+    
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    
+
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
 
     def get(self, key: int) -> int:
@@ -20,17 +30,6 @@ class LRUCache:
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-    
-
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
-    
-
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
         
 
     def put(self, key: int, value: int) -> None:
