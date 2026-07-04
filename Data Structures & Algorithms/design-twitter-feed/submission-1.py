9bd518837791934bdb425c7f69dfff36c0f88145
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-04 13:34:41 -0500

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.py
@@ -0,0 +1,26 @@
+class Twitter:
+
+    def __init__(self):
+        self.count = 0
+        self.tweets = defaultdict(lambda : deque(maxlen=10))
+        self.follows = defaultdict(set)
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].appendleft((self.count, tweetId))
+        self.count += 1
+        
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        posts = (self.tweets[id] for id in {userId} | self.follows[userId])
+        mergedPosts = heapq.merge(*posts, reverse=True)
+        return [tweetId for _, tweetId in islice(mergedPosts, 10)]
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.follows[followerId]:
+            self.follows[followerId].remove(followeeId)
