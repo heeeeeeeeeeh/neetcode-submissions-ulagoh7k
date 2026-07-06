f729027b17ac356cf4da13bac2d75701766bb2ba
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-05 20:25:19 -0500

    Update: reorder-linked-list - submission-0

--- Data Structures & Algorithms/reorder-linked-list/submission-0.py
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -8,9 +8,8 @@ class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         slow, fast = head, head.next
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-        
+            slow = slow.next
         second = slow.next
         prev = slow.next = None
         while second:
@@ -18,8 +17,7 @@ class Solution:
             second.next = prev
             prev = second
             second = tmp
-        
-        first, second =head, prev
+        first, second = head, prev
         while second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
