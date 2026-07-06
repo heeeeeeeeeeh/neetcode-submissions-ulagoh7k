9b2f67efd8c8666ce2494a9fc77919f0eb1fd190
Silvestre Espinoza <silvestre.espinoza@proton.me>
2026-07-05 20:53:21 -0500

    Update: depth-of-binary-tree - submission-1

--- Data Structures & Algorithms/depth-of-binary-tree/submission-1.py
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.py
@@ -5,10 +5,13 @@
 #         self.left = left
 #         self.right = right
 
-
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        def dfs(root):
+            if not root:
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            return 1 + max(left, right)
+        return dfs(root)
